287ca993372ac3bcf5ebfcce2672cf8565635f40
Francianeny <nirinasoabozy@yahoo.fr>
2024-05-10 11:39:33 +0200

    uppercase py

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -2,6 +2,6 @@
 
 def uppercase(s):
     for char in s:
-        upper_char = chr(ord(char) - 32) if 97 <= ord(char) <= 122 else char
+        upper_char = chr(ord(char) - 32) if 65 <= ord(char) <= 90 else char
         print(upper_char, end="")
     print()
