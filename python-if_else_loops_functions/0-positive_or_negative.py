1dd7f64bbba605d36c363febcf124bc95da0841b
Francianeny <nirinasoabozy@yahoo.fr>
2024-05-12 17:19:05 +0200

    positive or negative

--- python-if_else_loops_functions/0-positive_or_negative.py
+++ python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
 
-numbers = [5, -7, 0, 10, -5]  # Liste des nombres prédéfinis
+import random
 
-for number in numbers:
-    if number > 0:
-        print(f"{number} is positive")
-    elif number == 0:
-        print("0 is zero")
-    else:
-        print(f"{number} is negative")
+# Initialisation du générateur de nombres aléatoires avec une valeur fixe
+random.seed(0)
+
+number = random.randint(-10, 10)
+
+if number > 0:
+    print(f"{number} is positive")
+elif number == 0:
+    print("0 is zero")
+else:
+    print(f"{number} is negative")
