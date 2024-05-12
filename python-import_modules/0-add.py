92c6bd3ba9b371150af444c80977e190fe97f329
Francianeny <nirinasoabozy@yahoo.fr>
2024-05-12 17:19:39 +0000

    0 add

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-# Assign values to variables a and b
+
+# Assigner les valeurs aux variables a et b
 a = 1
 b = 2
 
-# Import the add function from add_0.py
+# Importer la fonction add depuis add_0.py
 add_module = __import__('add_0')
 add = add_module.add
 
-# Print the result of the addition
+# Afficher le résultat de l'addition
 print('{} + {} = {}'.format(a, b, add(a, b)))
