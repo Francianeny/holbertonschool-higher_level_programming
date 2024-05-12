88cabf91e39334f26acaf7dab9b865985dd6a420
Francianeny <nirinasoabozy@yahoo.fr>
2024-05-12 17:23:48 +0200

    positive or negative

--- python-if_else_loops_functions/0-positive_or_negative.py
+++ python-if_else_loops_functions/0-positive_or_negative.py
@@ -2,14 +2,14 @@
 
 import random
 
-# Initialisation du nombre à zéro
-number = 0
+# Générer un nombre aléatoire entre -10 et 10
+number = random.randint(-10, 10)
 
-# Générer un nombre aléatoire jusqu'à ce qu'il soit différent de zéro
-while number == 0:
-    number = random.randint(-10, 10)
-
-if number > 0:
-    print(f"{number} is positive")
+# Vérifier si le nombre est différent de zéro
+if number != 0:
+    if number > 0:
+        print(f"{number} is positive")
+    else:
+        print(f"{number} is negative")
 else:
-    print(f"{number} is negative")
+    print("0 is zero")
