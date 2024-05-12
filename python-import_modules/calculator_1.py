1c6715768f20409efb82e44daf85987d42213c56
Francianeny <nirinasoabozy@yahoo.fr>
2024-05-12 18:17:50 +0000

    calculator

--- python-import_modules/calculator_1.py
+++ python-import_modules/calculator_1.py
@@ -1,52 +1,14 @@
 #!/usr/bin/python3
-def add(a, b):
-    """My addition function
 
-    Args:
-        a: first integer
-        b: second integer
+# Importer les fonctions du module calculator_1.py
+from calculator_1 import add, sub, mul, div
 
-    Returns:
-        The return value. a + b
-    """
-    return (a + b)
+# Définir les valeurs des variables a et b
+a = 10
+b = 5
 
-
-def sub(a, b):
-    """My subtraction function
-
-    Args:
-        a: first integer
-        b: second integer
-
-    Returns:
-        The return value. a - b
-    """
-    return (a - b)
-
-
-def mul(a, b):
-    """My multiplication function
-
-    Args:
-        a: first integer
-        b: second integer
-
-    Returns:
-        The return value. a * b
-    """
-    return (a * b)
-
-
-def div(a, b):
-    """My division function
-
-    Args:
-        a: first integer
-        b: second integer
-
-    Returns:
-        The return value. a / b
-    """
-    return int(a / b)
-
+# Appeler chaque fonction importée et imprimer les résultats
+print("{} + {} = {}".format(a, b, add(a, b)))
+print("{} - {} = {}".format(a, b, sub(a, b)))
+print("{} * {} = {}".format(a, b, mul(a, b)))
+print("{} / {} = {}".format(a, b, div(a, b)))
