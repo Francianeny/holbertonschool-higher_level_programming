814ba1ff8ecd668ef825e9dfc6867c6a85f53183
Francianeny <nirinasoabozy@yahoo.fr>
2024-05-13 12:57:23 +0200

    1-calculation.py

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -7,8 +7,8 @@ if __name__ == "__main__":
     b = 5
 
 # Appeler les fonctions et afficher les résultats
-print("{} + {} = {:.0f}".format(a, b, add(a, b)))
-print("{} - {} = {:.0f}".format(a, b, sub(a, b)))
-print("{} * {} = {:.0f}".format(a, b, mul(a, b)))
-print("{} / {} = {:.0f}".format(a, b, div(a, b)))
+print("{} + {} = {:.0f}".format(a, b, calculator_1.add(a, b)))
+print("{} - {} = {:.0f}".format(a, b, calculator_1.sub(a, b)))
+print("{} * {} = {:.0f}".format(a, b, calculator_1.mul(a, b)))
+print("{} / {} = {:.0f}".format(a, b, calculator_1.div(a, b)))
 
