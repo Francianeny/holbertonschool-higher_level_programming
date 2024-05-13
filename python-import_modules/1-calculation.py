915007a00f55d78177f9f32a8e1201e5f68cc2f8
Francianeny <nirinasoabozy@yahoo.fr>
2024-05-13 13:18:13 +0200

    1-calculation.py

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
 
 if __name__ == "__main__":
-    import calculator_1
+    from calculator_1 import add, sub, mul, div
 
     a = 10
     b = 5
-# Appeler les fonctions et afficher les résultats
-print("{} + {} = {:.0f}".format(a, b, calculator_1.add(a, b)))
-print("{} - {} = {:.0f}".format(a, b, calculator_1.sub(a, b)))
-print("{} * {} = {:.0f}".format(a, b, calculator_1.mul(a, b)))
-print("{} / {} = {:.0f}".format(a, b, calculator_1.div(a, b)))
+a = 10
+b = 5
+print("{} + {} = {:.0f}".format(a, b, add(a, b)))
+print("{} - {} = {:.0f}".format(a, b, sub(a, b)))
+print("{} * {} = {:.0f}".format(a, b, mul(a, b)))
+print("{} / {} = {:.0f}".format(a, b, div(a, b)))
