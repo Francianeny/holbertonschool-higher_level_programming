2c1aa9abd86166dde17d61acc194567c0dc9e83e
Francianeny <nirinasoabozy@yahoo.fr>
2024-05-13 15:04:21 +0200

    2-args.py

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -6,7 +6,7 @@ def print_arguments():
     num_args = len(sys.argv) - 1
     if num_args == 0:
         print("0 argument.")
-        print(":")
+
     else:
         print("{} argument{}:".format(num_args, 's' if num_args > 1 else ''))
         for i, arg in enumerate(sys.argv[1:], 1):
