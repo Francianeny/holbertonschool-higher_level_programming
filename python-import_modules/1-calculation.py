0d753aba720310b520e598f20caa1c2921955d35
Francianeny <nirinasoabozy@yahoo.fr>
2024-05-13 13:21:26 +0200

    1-calculation.py

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -6,9 +6,7 @@ if __name__ == "__main__":
 
     a = 10
     b = 5
-a = 10
-b = 5
-print("{} + {} = {:.0f}".format(a, b, add(a, b)))
-print("{} - {} = {:.0f}".format(a, b, sub(a, b)))
-print("{} * {} = {:.0f}".format(a, b, mul(a, b)))
-print("{} / {} = {:.0f}".format(a, b, div(a, b)))
+    print("{} + {} = {:.0f}".format(a, b, add(a, b)))
+    print("{} - {} = {:.0f}".format(a, b, sub(a, b)))
+    print("{} * {} = {:.0f}".format(a, b, mul(a, b)))
+    print("{} / {} = {:.0f}".format(a, b, div(a, b)))
