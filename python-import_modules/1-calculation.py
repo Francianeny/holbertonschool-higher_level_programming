734f25732bc4bc758570aa68fe5ef63453d9ce1d
Francianeny <nirinasoabozy@yahoo.fr>
2024-05-13 13:06:28 +0200

    1-calculation.py

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
 
-import calculator_1
-
 if __name__ == "__main__":
+    import calculator_1
+
     a = 10
     b = 5
 
-# Appeler les fonctions et afficher les résultats
-print("{} + {} = {:.0f}".format(a, b, calculator_1.add(a, b)))
-print("{} - {} = {:.0f}".format(a, b, calculator_1.sub(a, b)))
-print("{} * {} = {:.0f}".format(a, b, calculator_1.mul(a, b)))
-print("{} / {} = {:.0f}".format(a, b, calculator_1.div(a, b)))
+    addition_result = calculator_1.add(a, b)
+    subtraction_result = calculator_1.sub(a, b)
+    multiplication_result = calculator_1.mul(a, b)
+    division_result = calculator_1.div(a, b)
 
+    print(addition_result)
+    print(subtraction_result)
+    print(multiplication_result)
+    print(division_result)
