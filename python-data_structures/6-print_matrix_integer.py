9eb389b5f38a0eed32cea1a09dfdd7f48f1837c9
Francianeny <nirinasoabozy@yahoo.fr>
2024-05-14 19:50:42 +0200

    6-print_matrix_integer.py

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -5,4 +5,4 @@ def print_matrix_integer(matrix=[[]]):
         for i in range(len(row)):
             # Print each element followed by a space
             print("{:d}".format(row[i]), end=" ")
-            print() # Move to the next line after printing each row
+        print("")
