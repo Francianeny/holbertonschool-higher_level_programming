228f5b9f8c246e52bd0303b32a06704da5f12505
Francianeny <nirinasoabozy@yahoo.fr>
2024-05-15 20:38:29 +0200

    7-add_tuple.py

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 
 def add_tuple(tuple_a=(), tuple_b=()):
-    # Extract the first two integers from each tuple or use 0 for missing elements
     a = tuple_a[:2] + (0,) * (2 - len(tuple_a))
     b = tuple_b[:2] + (0,) * (2 - len(tuple_b))
 
-    # Add corresponding elements together
     result = (a[0] + b[0], a[1] + b[1])
 
     return result
