4ede38a9bf150e845d61e3c0e741aef26a02662f
Francianeny <nirinasoabozy@yahoo.fr>
2024-05-15 19:41:29 +0200

    6-print_matrix_integer.py

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,11 +2,8 @@
 
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        for i in range(len(row)):
-            # Print each element followed by a space
-            print("{:d}".format(row[i]), end=" ")
-            # Check if the current column is the third one
-            if (i + 1) % 3 == 0 and i != len(row) - 1:
-             print(end="")
-        print("")  # Move print("") outside the inner loop to print a new line after each row
+        for num in row:
+            print("{:d}".format(num), end="")
+        print("")
+
 
