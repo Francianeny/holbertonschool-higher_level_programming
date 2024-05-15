6cf6b79389dc37240acf80524cc775db3a061d0f
Francianeny <nirinasoabozy@yahoo.fr>
2024-05-15 20:40:16 +0200

    8-multiple_returns.py

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 
 def multiple_returns(sentence):
-    # Check if the sentence is empty
     if len(sentence) == 0:
-        return (0, None)  # Return (0, None) if the sentence is empty
+        return (0, None)
     else:
         return (len(sentence), sentence[0])  # Return tuple with length and first character
