355b5c72f339c68af325b717a744a680146f3794
Francianeny <nirinasoabozy@yahoo.fr>
2024-05-16 13:35:22 +0200

    1-search_replace.py

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,9 +1,9 @@
-def search_replace(my_list, search, replace):
+#!/usr/bin/python3
 
+def search_replace(my_list, search, replace):
     new_list = []
 
     for item in my_list:
-        
         if item == search:
             new_list.append(replace)
         else:
