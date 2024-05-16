8da766678700ac61a393a2d754a3d09991b2b399
Francianeny <nirinasoabozy@yahoo.fr>
2024-05-16 21:41:14 +0200

    4-hidden_discovery.py

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,16 @@
 #!/usr/bin/python3
-import hidden_4
+import os
+import imp
 
 if __name__ == "__main__":
-    names = dir(hidden_4)
-    sorted_name = sorted(name for name in names if not name.startswith("__"))
-    for name in sorted_name:
-        print(name)
+    chemin_module = '/tmp/hidden_4.pyc'
+
+    if os.path.exists(chemin_module):
+        nom_module = os.path.splitext(os.path.basename(chemin_module))[0]
+        module = imp.load_compiled(nom_module, chemin_module)
+
+        noms = [nom for nom in dir(module) if not nom.startswith("__")]
+        noms.sort()  # Trier les noms par ordre alphabétique
+
+        for nom in noms:
+            print(nom)
