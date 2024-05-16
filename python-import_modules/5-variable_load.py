88ab0f4fc33fa2cc577449ad00333cfd43fc10ec
Francianeny <nirinasoabozy@yahoo.fr>
2024-05-16 21:08:19 +0200

    5-variable_load.py

--- python-import_modules/5-variable_load.py
+++ python-import_modules/5-variable_load.py
@@ -1,13 +1,6 @@
 #!/usr/bin/python3
 
-def main():
-    # Exécute le contenu du fichier variable_load_5.py
-    with open('variable_load_5.py', 'r') as file:
-        code = compile(file.read(), 'variable_load_5.py', 'exec')
-        exec(code, globals())
+if __name__ == '__main__':
+    from variable_load_5 import a
+    print("{}".format(a))
 
-    # Imprime la valeur de la variable 'a'
-    print(a)
-
-if __name__ == "__main__":
-    main()
