e8ef8f20e121cebbfef346a9eb7bedf0c222ce19
Francianeny <nirinasoabozy@yahoo.fr>
2024-05-16 13:38:38 +0200

    0-square_matrix_simple.py

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -3,9 +3,10 @@
 def square_matrix_simple(matrix=[]):
 
     result = []
+
     for row in matrix:
         new_row = []
         for num in row:
             new_row.append(num ** 2)
-        result.append(new_row)
+            result.append(new_row)
     return result
