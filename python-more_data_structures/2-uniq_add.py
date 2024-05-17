3bf62e9a894c5c12ef57c0fa5404a8da397f35c9
Francianeny <nirinasoabozy@yahoo.fr>
2024-05-17 22:51:22 +0200

    2-uniq_add.py

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 
 def uniq_add(my_list=[]):
-    unique_integers = set()
-    for num in my_list:
-        unique_integers.add(num)
-    return sum(unique_integers)
+    result = sum(set(my_list))
+    return result
 
 
