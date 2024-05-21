ed3fc9b0b4b7471b03f891dacc195294fccb3850
Francianeny <nirinasoabozy@yahoo.fr>
2024-05-21 19:58:23 +0200

    0-add_integer.py

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+def add_integer(a, b=98):
+    """
+    add two integers of float casted to integers.
+
+    Parameters:
+    a (int or float): the frist number.
+    b (int or float): the seconde number, default is 98.
+
+    Raises:
+    TypeError: If a or b are not integers or float.
+
+    Return:
+    int: The sum of a and b casted to integers.
+    """
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
