7a8886bb37b4b423579caecd07c3ecadf53f6675
Francianeny <nirinasoabozy@yahoo.fr>
2024-05-24 01:15:06 +0200

    6-base_geometry.py

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""""Class BaseGemotry"""
+
+
+class BaseGeometry:
+    """empty class BaseGeometry"""
+
+    def area(self):
+        """that raises an Exception with the message
+        area() is not implemented"""
+        raise Exception("area() is not implemented")
