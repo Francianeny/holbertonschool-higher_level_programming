4b471b4ed7dd7c97d79293fd052a22202001cd4d
Francianeny <nirinasoabozy@yahoo.fr>
2024-05-23 13:57:06 +0200

    5-text_indentation.py

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,15 +1,27 @@
 #!/usr/bin/python3
-"""define text_indetation function"""
+"""Module for text_indentation method."""
 
 
 def text_indentation(text):
-    """prints a text with 2 new lines after
-    each of these characters: ., ? and :"""
+    """Method for adding 2 new lines after '.?:' chars.
+
+    Args:
+        text: The str text.
+
+    Raises:
+        TypeError: If text is not a str.
+    """
     if not isinstance(text, str):
         raise TypeError("text must be a string")
 
-    punctuations = ['.', '?', ':']
-    for char in text:
-        print(char, end='')
-        if char in punctuations:
-            print("{}".format('\n\n'), end='')
+    for delim in ".?:":
+        # print(delim, text.split(delim))
+        text = (delim + "\n\n").join(
+            [line.strip(" ") for line in text.split(delim)])
+
+    print(text, end="")
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/5-text_indentation.txt")
