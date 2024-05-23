db28540af60a1fe9518dfd5ae717979902f78be4
Francianeny <nirinasoabozy@yahoo.fr>
2024-05-24 01:50:51 +0200

    task_00_abc.py

---
+++ python-abc/task_00_abc.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+from abc import ABC, abstractmethod
+
+# Abstract base class
+class Animal(ABC):
+
+    @abstractmethod
+    def sound(self):
+        pass
+
+# Subclass Dog
+class Dog(Animal):
+
+    def sound(self):
+        return "Bark"
+
+# Subclass Cat
+class Cat(Animal):
+
+    def sound(self):
+        return "Meow"
