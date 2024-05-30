a6f32c5f394768a2c376a93954676ba9c2d5f973
Francianeny <nirinasoabozy@yahoo.fr>
2024-05-30 11:22:45 +0200

    3-to_json_string.py

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3*
+"""dumps funciton for converting the obj to string """
+
+
+import json
+"""from object to string"""
+
+
+def to_json_string(my_obj):
+    """return json representation from obj to string"""
+    return json.dumps(my_obj)
