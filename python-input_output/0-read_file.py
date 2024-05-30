43f70c607f2475f88b0f9053f04f2b56d99e0dea
Francianeny <nirinasoabozy@yahoo.fr>
2024-05-30 15:08:58 +0200

    0-read_file.py

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-"""read_file module"""
+"""contains the file Read"""
+
 
 def read_file(filename=""):
-    """Function that reads a text file (UTF8) and prints it to stdout"""
+    """function that reads a text file (UTF8) and prints it to stdout"""
     with open(filename, 'r', encoding='utf-8') as f:
-        lines = f.read()
-        print(lines, end="")
+        print(f.read(), end='')
