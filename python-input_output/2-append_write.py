7742ff737ae0003017a0f391052d6d60a8ea8608
Francianeny <nirinasoabozy@yahoo.fr>
2024-05-30 11:19:36 +0200

    2-append_write.py

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3*
+"""append_write"""
+
+
+def append_write(filename="", text=""):
+    """ appends a string at the end of a text file (UTF8)"""
+    length = 0
+    with open(filename, "a") as myfile:
+        length = myfile.write(text)
+        myfile.close()
+    return length
