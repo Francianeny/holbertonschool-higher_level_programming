e89ee5a2a2cb0df88296d752abb0b083b645ca8a
Francianeny <nirinasoabozy@yahoo.fr>
2024-05-30 15:04:07 +0200

    0-read_file.py

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -6,4 +6,4 @@ def read_file(filename=""):
     """function that reads a text file (UTF8) and prints it to stdout:"""
     with open(filename) as f:
         lines = f.read()
-        print(lines, end="") # i
+        print(lines, end="")
