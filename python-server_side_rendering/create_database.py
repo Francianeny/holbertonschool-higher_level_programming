e9895456e9fa1c5bae82da5a4edbafb9441e6026
Francianeny <nirinasoabozy@yahoo.fr>
2024-07-18 00:46:50 +0200

    create_database.py

--- python-server_side_rendering/create_database.py
+++ python-server_side_rendering/create_database.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 
- import sqlite3
+import sqlite3
 
-   def create_database():
-       conn = sqlite3.connect('products.db')
-       cursor = conn.cursor()
-       cursor.execute('''
+def create_database():
+    conn = sqlite3.connect('products.db')
+    cursor = conn.cursor()
+    cursor.execute('''
            CREATE TABLE IF NOT EXISTS Products (
                id INTEGER PRIMARY KEY,
                name TEXT NOT NULL,
@@ -13,14 +13,14 @@
                price REAL NOT NULL
            )
        ''')
-       cursor.execute('''
+    cursor.execute('''
            INSERT INTO Products (id, name, category, price)
            VALUES
            (1, 'Laptop', 'Electronics', 799.99),
            (2, 'Coffee Mug', 'Home Goods', 15.99)
        ''')
-       conn.commit()
-       conn.close()
+    conn.commit()
+    conn.close()
 
-   if __name__ == '__main__':
+if __name__ == '__main__':
        create_database()
